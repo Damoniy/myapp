e77dae9c6767248fbc693ad0a13cfb0a9f08f920
Emerson Przybyloviecz <emersonp199@gmail.com>
2021-12-30 23:11:01 -0300

    Adicionado menu de contexto para ação de remover elemento da lista de alunos.

--- app/src/main/java/com/damoniy/myapp/ui/activities/StudentListActivity.kt
+++ app/src/main/java/com/damoniy/myapp/ui/activities/StudentListActivity.kt
@@ -2,6 +2,8 @@ package com.damoniy.myapp.ui.activities
 
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextMenu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -17,19 +19,37 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class StudentListActivity: AppCompatActivity() {
 
     private lateinit var arrayAdapter: ArrayAdapter<Student>
+    private val studentDAO = Persistence.studentDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.title = "Student's List"
         this.setContentView(R.layout.activity_student_list)
         this.createButton()
+        this.configureListView()
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menu!!.add("Remover")
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val menuInfo = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        val student: Student = arrayAdapter.getItem(menuInfo.position)!!
+        this.removeStudent(student)
+        return super.onContextItemSelected(item)
     }
 
     override fun onResume() {
         super.onResume()
-        this.configureListView()
+        update()
     }
 
+    private fun update() {
+        this.arrayAdapter.clear()
+        this.arrayAdapter.addAll(studentDAO.getObjectList())
+    }
 
     private fun createButton() {
         val button: FloatingActionButton = findViewById(R.id.activity_student_list_add_btn)
@@ -37,22 +57,23 @@ class StudentListActivity: AppCompatActivity() {
     }
 
     private fun configureListView() {
-        val students = Persistence.studentDAO.getObjectList()
         val studentsListView: ListView = findViewById(R.id.activity_student_list)
-        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, students)
-        studentsListView.adapter = arrayAdapter
-        studentsListView.onItemClickListener = StudentListViewListener(this)
-        studentsListView.setOnItemLongClickListener { adapterView, view, i, l -> this.onItemLongClick(adapterView, view, i, l) }
+        initAdapter(studentsListView)
+        initListeners(studentsListView)
+        registerForContextMenu(studentsListView)
     }
 
-    private fun onItemLongClick(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long): Boolean {
-        val student = captureStudent(adapterView, position)
+    private fun initListeners(studentsListView: ListView) {
+        studentsListView.onItemClickListener = StudentListViewListener(this)
+    }
+
+    private fun initAdapter(studentsListView: ListView) {
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1)
+        studentsListView.adapter = arrayAdapter
+    }
+
+    private fun removeStudent(student: Student) {
         Persistence.studentDAO.remove(student)
         this.arrayAdapter.remove(student)
-        return true
-    }
-
-    private fun captureStudent(adapterView: AdapterView<*>?, position: Int): Student {
-        return adapterView!!.getItemAtPosition(position) as Student
     }
 }
